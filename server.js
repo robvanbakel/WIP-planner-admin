58a2a1ee19a4d91be623f6a1b6693c7e6dcc8ca0
Rob van Bakel <robbakel@gmail.com>
2021-09-20 13:15:18 +0200

    Create route for getting schedules for single employee

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const { v4: uuidv4 } = require("uuid")
 const express = require("express")
 const cors = require("cors")
-const cron = require('node-cron');
+const cron = require("node-cron")
 const app = express()
 
 app.use(cors())
@@ -14,7 +14,7 @@ const shiftDatabase = require("./helpers/shiftDatabase")
 
 // Every monday at midnight, move database
 // with demo content to current week
-cron.schedule('0 0 * * 1', () => shiftDatabase());
+cron.schedule("0 0 * * 1", () => shiftDatabase())
 
 app.post("/createNewUser", async (req, res) => {
   try {
@@ -23,7 +23,7 @@ app.post("/createNewUser", async (req, res) => {
       password: uuidv4(),
     })
 
-    res.send({uid})
+    res.send({ uid })
   } catch (err) {
     res.send(err)
   }
@@ -50,6 +50,32 @@ app.get("/feed/:id", async (req, res) => {
   res.send(icsContent)
 
   console.log(`Served feed to ${uid} at ${new Date().toLocaleString()}`)
+})
+
+app.get("/getSchedules/:id", async (req, res) => {
+  const uid = req.params.id
+
+  let schedules = {}
+
+  const snapshot = await db.collection("schedules").get()
+  snapshot.forEach((doc) => {
+    const week = doc.data()
+
+    if (week[uid]) {
+      schedules[doc.id] = week[uid].map((day) => {
+        if (day) {
+          return {
+            start: day.start,
+            break: day.break,
+            end: day.end,
+            place: day.place,
+          }
+        }
+      })
+    }
+  })
+
+  res.send(schedules)
 })
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
