7baeed8ae7441b1b577b7e37aa6fea2a951f2e98
Rob van Bakel <robbakel@gmail.com>
2021-09-24 20:44:41 +0200

    Add notes to calendar subscription feed

--- helpers/parse.js
+++ helpers/parse.js
@@ -1,21 +1,17 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid")
 
 const getDatesFromWeekId = require("./getDatesFromWeekId")
 
 const parse = (schedules) => {
-  
   // Open string that will be served as a calendar stream (.ics)
   let icsContent = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Rob van Bakel//Planner//EN"
 
   for (const weekId in schedules) {
-
     // Establish dates from weekId
     const dates = getDatesFromWeekId(weekId)
 
     schedules[weekId].forEach((shift, index) => {
-
       if (shift) {
-
         // Format date
         const date = dates[index].getDate().toString().padStart(2, "0")
         const month = (dates[index].getMonth() + 1).toString().padStart(2, "0")
@@ -28,7 +24,7 @@ const parse = (schedules) => {
         icsContent = icsContent.concat(`\nUID:${uuidv4()}`)
 
         // Add timestamp to event in .ics content
-        icsContent = icsContent.concat(`\nDTSTAMP:${new Date().toISOString().replace(/[-:]/g,'').split('.')[0]}`)
+        icsContent = icsContent.concat(`\nDTSTAMP:${new Date().toISOString().replace(/[-:]/g, "").split(".")[0]}`)
 
         // Add date and time string to .ics content
         icsContent = icsContent.concat(`\nDTSTART:${year}${month}${date}T${shift.start}00\nDTEND:${year}${month}${date}T${shift.end}00`)
@@ -38,6 +34,11 @@ const parse = (schedules) => {
 
         // Add location to .ics content
         icsContent = icsContent.concat(`\nLOCATION:Stationsplein\\, 5611 AD Eindhoven`)
+
+        // If note is present, add notes to .ics content
+        if (shift.notes) {
+          icsContent = icsContent.concat(`\nDESCRIPTION:${shift.notes}`)
+        }
 
         // Close event in .ics content
         icsContent = icsContent.concat(`\nEND:VEVENT`)
