02ab5fa72ba6d0d48a73e2f1aeff29193472224c
Rob van Bakel <robbakel@gmail.com>
2021-09-27 11:01:22 +0200

    Implement async functions to enable shifting multiple weeks

--- helpers/shiftDatabase.js
+++ helpers/shiftDatabase.js
@@ -1,40 +1,47 @@
 require("dotenv").config()
 
-const getNextWeekId = require('./getNextWeekId')
+const getNextWeekId = require("./getNextWeekId")
 
 const { db } = require("../firebase")
 
 const shiftDatabase = async () => {
 
-  let oldDatabase = {}
+  // Save old version of database
 
   const snapshot = await db.collection("schedules").get()
 
-  // Save old version of database
+  let oldDatabase = {}
 
   snapshot.forEach((doc) => {
-
     oldDatabase[doc.id] = doc.data()
-  
   })
 
-  // Create new version of database
+  // Function to delete old weekIds
 
-  for (const weekId in oldDatabase) {
-
-    const nextWeekId = getNextWeekId(weekId)
-    const schedules = oldDatabase[weekId]
-    
-    // Add new weekId to database
-    await db.collection("schedules").doc(nextWeekId).set(schedules)
-    
-    // Delete old weekId
-    await db.collection("schedules").doc(weekId).delete()
-
-    console.log(`Shifted week ${weekId} -> ${nextWeekId}`)
-
+  const deleteWeeks = async () => {
+    for (const weekId in oldDatabase) {
+      await db.collection("schedules").doc(weekId).delete()
+    }
   }
-  
+
+  // Function to create shifted weekIds
+
+  const createWeeks = async () => {
+    for (const weekId in oldDatabase) {
+      const nextWeekId = getNextWeekId(weekId)
+      const schedules = oldDatabase[weekId]
+
+      await db.collection("schedules").doc(nextWeekId).set(schedules)
+
+      console.log(`Shifted week ${weekId} -> ${nextWeekId}`)
+    }
+  }
+
+  // Call functions
+
+  await deleteWeeks()
+
+  await createWeeks()
 }
 
 module.exports = shiftDatabase
