66105938e9ad5993e40127f0ddfd79125c59295d
Rob van Bakel <robbakel@gmail.com>
2021-10-16 12:30:52 +0200

    Change email address sending account activation links

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,19 +1,21 @@
 require("dotenv").config()
 const sgMail = require("@sendgrid/mail")
 
-const activationMailTemplate = require('./activationMailTemplate')
+const activationMailTemplate = require("./activationMailTemplate")
 
 sgMail.setApiKey(process.env.SENDGRID_KEY)
 
 const sendMail = ({ activationToken, email, firstName }) => {
-  
   const activationLink = `https://demo.sparkscheduler.com/auth?activationToken=${activationToken}`
 
   sgMail.send({
     to: email,
-    from: "info@robvanbakel.nl",
-    subject: `Spark: Activate your account`,
-    html: activationMailTemplate({activationLink,firstName}),
+    from: {
+      email: "info@sparkscheduler.com",
+      name: "Spark",
+    },
+    subject: `Activate your account`,
+    html: activationMailTemplate({ activationLink, firstName }),
   })
 }
 
