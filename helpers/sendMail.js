400be6f8c1bafac7053b9fbb2cc33db7fcc8ed76
Rob van Bakel <robbakel@gmail.com>
2021-12-07 22:24:42 +0100

    Update account activation link

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -8,7 +8,7 @@ sgMail.setApiKey(process.env.SENDGRID_KEY)
 const sendMail = ({ activationToken, email, firstName }) => {
 
   // Create link with query param holding activation token
-  const activationLink = `https://demo.sparkscheduler.com/auth?activationToken=${activationToken}`
+  const activationLink = `https://app.sparkscheduler.com/auth?activationToken=${activationToken}`
 
   // Send activation mail to user using SendGrid API
   sgMail.send({
