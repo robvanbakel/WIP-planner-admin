97d5df1e2ec2af9fa85c7dc97d14d91389e9ef00
Rob van Bakel <robbakel@gmail.com>
2022-05-15 14:53:42 +0200

    Add Day.js plugins

--- dayjs/index.js
+++ dayjs/index.js
@@ -1,7 +1,19 @@
 const dayjs = require('dayjs')
 
 const localizedFormat = require('dayjs/plugin/localizedFormat')
+const advancedFormat = require('dayjs/plugin/advancedFormat')
+const isoWeek = require('dayjs/plugin/isoWeek')
+
+const weekId = (option, dayjsClass) => {
+  const proto = dayjsClass.prototype;
+  proto.weekId = function () {
+    return this.format('GGGG-WW');
+  };
+};
 
 dayjs.extend(localizedFormat)
+dayjs.extend(advancedFormat)
+dayjs.extend(isoWeek);
+dayjs.extend(weekId);
 
 module.exports = dayjs
