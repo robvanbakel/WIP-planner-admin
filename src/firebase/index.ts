99d7d84f72c20816ca59d09ead6fa72e4ddf6330
Rob van Bakel <robbakel@gmail.com>
2022-12-31 10:19:21 +0100

    Convert Firebase init to TypeScript

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,24 +1,25 @@
-const admin = require('firebase-admin');
+import admin from 'firebase-admin';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const serviceAccount = {
   type: process.env.GOOGLE_type,
   project_id: process.env.GOOGLE_project_id,
   private_key_id: process.env.GOOGLE_private_key_id,
-  private_key: process.env.GOOGLE_private_key.replace(/\\n/g, '\n'),
+  private_key: (process.env.GOOGLE_private_key as string).replace(/\\n/g, '\n'),
   client_email: process.env.GOOGLE_client_email,
   client_id: process.env.GOOGLE_client_id,
   auth_uri: process.env.GOOGLE_auth_uri,
   token_uri: process.env.GOOGLE_token_uri,
   auth_provider_x509_cert_url: process.env.GOOGLE_auth_provider_x509_cert_url,
   client_x509_cert_url: process.env.GOOGLE_client_x509_cert_url,
-};
+} as admin.ServiceAccount;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_database_url,
 });
 
-const auth = admin.auth();
-const db = admin.firestore();
-
-module.exports = { auth, db };
+export const auth = admin.auth();
+export const db = admin.firestore();
